36904c678cb864cef20486077150fc6a315dbec7
Anurag Raj <adityanv8@gmail.com>
2025-08-25 07:38:32 +0530

    changes

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -51,9 +51,9 @@ const Contact = () => {
             <div className="social-link">
               <h2>Phone</h2>
               <div className="w-full h-px my-2 bg-white/30" />
-              <p className="text-xl lowercase md:text-2xl lg:text-3xl">
+              {/* <p className="text-xl lowercase md:text-2xl lg:text-3xl">
                 +91 7464954996
-              </p>
+              </p> */}
             </div>
             <div className="social-link">
               <h2>Social Media</h2>
