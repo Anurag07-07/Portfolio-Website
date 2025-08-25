eed8138141988c4e075ba7874265fb6e803d9043
Anurag Raj <adityanv8@gmail.com>
2025-08-25 07:26:43 +0530

    Added Project

--- src/constants/index.js
+++ src/constants/index.js
@@ -19,67 +19,67 @@ export const servicesData = [
       },
     ],
   },
-  {
-    title: "DevOps & Cloud Solutions",
-    description:
-      "Deploying software shouldn't be a gamble. I automate infrastructure, enforce security, and leverage cloud platforms (AWS/Azure) to keep your app running smoothly—24/7, at any scale.",
-    items: [
-      {
-        title: "CI/CD Pipelines",
-        description: "(GitHub Actions, Docker, Kubernetes)",
-      },
-      {
-        title: "Server Management ",
-        description: "(Linux, Nginx, Load Balancing)",
-      },
-      {
-        title: "Performance Tuning",
-        description: "(Caching, Compression, Lighthouse 90+ Scores)",
-      },
-    ],
-  },
-  {
-    title: "Security & Optimization",
-    description:
-      "Slow or hacked apps destroy trust. I harden security (XSS/SQLI protection, OAuth) and optimize bottlenecks so your app stays fast, safe, and scalable as you grow.",
-    items: [
-      {
-        title: "Code Audits",
-        description: "(Refactoring, Tech Debt Cleanup)",
-      },
-      {
-        title: "Pen Testing",
-        description: "(Vulnerability Assessments)",
-      },
-      {
-        title: "SEO Tech Stack",
-        description: "(SSR, Metadata, Structured Data)",
-      },
-    ],
-  },
-  {
-  title: "Web Apps & Web3",
-  description:
-    "I design and develop responsive, pixel-perfect Web Apps and Web3 experiences that users love—combining sleek UI/UX with blockchain functionality. Whether it's Solana or Ethereum, I build seamless interfaces that interact with smart contracts and wallets effortlessly.",
-  items: [
-    {
-      title: "Cross-Platform Apps",
-      description: "(Single codebase for iOS / Android / Web)",
-    },
-    {
-      title: "Progressive Web Apps (PWAs)",
-      description: "(Offline mode, Push Notifications, App-like feel)",
-    },
-    {
-      title: "Web3 Integration",
-      description: "(Wallet Connect, Smart Contracts, Solana / Ethereum DApps)",
-    },
-    {
-      title: "E-Commerce Solutions",
-      description: "(Checkout flows, Payment Gateways, Inventory APIs)",
-    },
-  ],
-}
+//   {
+//     title: "DevOps & Cloud Solutions",
+//     description:
+//       "Deploying software shouldn't be a gamble. I automate infrastructure, enforce security, and leverage cloud platforms (AWS/Azure) to keep your app running smoothly—24/7, at any scale.",
+//     items: [
+//       {
+//         title: "CI/CD Pipelines",
+//         description: "(GitHub Actions, Docker, Kubernetes)",
+//       },
+//       {
+//         title: "Server Management ",
+//         description: "(Linux, Nginx, Load Balancing)",
+//       },
+//       {
+//         title: "Performance Tuning",
+//         description: "(Caching, Compression, Lighthouse 90+ Scores)",
+//       },
+//     ],
+//   },
+//   {
+//     title: "Security & Optimization",
+//     description:
+//       "Slow or hacked apps destroy trust. I harden security (XSS/SQLI protection, OAuth) and optimize bottlenecks so your app stays fast, safe, and scalable as you grow.",
+//     items: [
+//       {
+//         title: "Code Audits",
+//         description: "(Refactoring, Tech Debt Cleanup)",
+//       },
+//       {
+//         title: "Pen Testing",
+//         description: "(Vulnerability Assessments)",
+//       },
+//       {
+//         title: "SEO Tech Stack",
+//         description: "(SSR, Metadata, Structured Data)",
+//       },
+//     ],
+//   },
+//   {
+//   title: "Web Apps & Web3",
+//   description:
+//     "I design and develop responsive, pixel-perfect Web Apps and Web3 experiences that users love—combining sleek UI/UX with blockchain functionality. Whether it's Solana or Ethereum, I build seamless interfaces that interact with smart contracts and wallets effortlessly.",
+//   items: [
+//     {
+//       title: "Cross-Platform Apps",
+//       description: "(Single codebase for iOS / Android / Web)",
+//     },
+//     {
+//       title: "Progressive Web Apps (PWAs)",
+//       description: "(Offline mode, Push Notifications, App-like feel)",
+//     },
+//     {
+//       title: "Web3 Integration",
+//       description: "(Wallet Connect, Smart Contracts, Solana / Ethereum DApps)",
+//     },
+//     {
+//       title: "E-Commerce Solutions",
+//       description: "(Checkout flows, Payment Gateways, Inventory APIs)",
+//     },
+//   ],
+// }
 
 ];
 export const projects = [
@@ -130,6 +130,40 @@ export const projects = [
     ],
     projectLink:"https://perfume-lilac.vercel.app/",
     githubLink:"https://github.com/Anurag07-07/perfume"
+  },
+  {
+    id: 4,
+    name: "Your Idea: Second Brain",
+    description:"I’ve built a Second Brain – a full-stack productivity app where you can capture, organize, and recall ideas, notes, and tasks instantly. It works like a personal knowledge assistant, helping you never forget important thoughts while keeping everything structured and searchable",
+    href: "",
+    image: "assets/projects/Screenshot 2025-08-18 062228.png",
+    bgImage: "/assets/backgrounds/map.jpg",
+    frameworks: [
+      { id: 1, name: "REACT JS" },
+      { id: 2, name: "TYPESCRIPT" },
+      { id: 3, name: "EXPRESS JS" },
+      { id: 4, name: "TAILWIND CSS" },
+      { id: 4, name: "GSAP" },
+    ],
+    projectLink:"https://secondbrain-2.onrender.com/",
+    githubLink:"https://github.com/Anurag07-07/SecondBrain"
+  },
+  {
+    id: 4,
+    name: "ExcilDraw:On Going",
+    href: "",
+    image: "assets/projects/Screenshot 2025-08-25 072210.png",
+    bgImage: "/assets/backgrounds/map.jpg",
+    frameworks: [
+      { id: 1, name: "NEXT JS" },
+      { id: 2, name: "TYPESCRIPT" },
+      { id: 3, name: "EXPRESS JS" },
+      { id: 4, name: "WEBSOCKET" },
+      { id: 5, name: "TURBOREPO" },
+      { id: 6, name: "PRISMA" },
+      { id: 7, name: "POSTGRES" },
+    ],
+    githubLink:"https://github.com/Anurag07-07/Drawapp"
   },
 ];
 export const socials = [
